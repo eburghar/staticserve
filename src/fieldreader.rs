82732695f8527aa4a0039cf15c470064962d50d1
Éric BURGHARD <eric@itsufficient.me>
2021-07-01 21:41:15 +0200

    cleanup

--- src/fieldreader.rs
+++ src/fieldreader.rs
@@ -4,100 +4,100 @@
 use actix_multipart::Field;
 use actix_web::web::{Buf, Bytes};
 use futures::{
-    io::{AsyncBufRead, AsyncRead},
-    stream::Stream,
-    task::{Context, Poll},
+	io::{AsyncBufRead, AsyncRead},
+	stream::Stream,
+	task::{Context, Poll},
 };
-use log::{debug, info};
 use pin_project_lite::pin_project;
 use std::{io::Write, pin::Pin};
 
 pin_project! {
-    pub struct FieldReader {
-	    // field is pinned because it is a stream (future) that needs to be polled
-        #[pin]
-        field: Field,
-        // chunk is an option of owned bytes that serve as a buffer for asyncbufread
-        chunk: Option<Bytes>,
-    }
+	pub struct FieldReader {
+		// field is pinned because it is a stream (future) that needs to be polled
+		#[pin]
+		field: Field,
+		// chunk is an option of owned bytes that serve as a buffer for asyncbufread
+		chunk: Option<Bytes>,
+	}
 }
 
 impl FieldReader {
-    pub fn new(field: Field) -> Self {
-        FieldReader { field, chunk: None }
-    }
+	pub fn new(field: Field) -> Self {
+		FieldReader { field, chunk: None }
+	}
 
-    /// Do we have a chunk and is it non-empty?
-    fn has_chunk(self: &Self) -> bool {
-	    self.chunk.as_ref().map_or_else(
-		    || false, |chunk| chunk.has_remaining())
-    }
+	/// Do we have a chunk and is it non-empty?
+	fn has_chunk(self: &Self) -> bool {
+		self.chunk
+			.as_ref()
+			.map_or_else(|| false, |chunk| chunk.has_remaining())
+	}
 }
 
 impl AsyncRead for FieldReader {
-    fn poll_read(
-        mut self: Pin<&mut Self>,
-        cx: &mut Context<'_>,
-        mut buf: &mut [u8],
-    ) -> Poll<Result<usize, std::io::Error>> {
-        debug!("poll_read into {} bytes", buf.len());
-        // self is consumed by poll_fill_buf() and we need to advance inner_buf after polling
-        // hence the use of as_mut() to get a new Pin<&mut FieldReader>)
-        let inner_buf = match self.as_mut().poll_fill_buf(cx) {
-            Poll::Ready(Ok(buf)) => buf,
-            Poll::Ready(Err(err)) => return Poll::Ready(Err(err)),
-            Poll::Pending => return Poll::Pending,
-        };
-        // fill buf entirely with field's chunk or fill partially with field's chunk remaining data
-        let len = std::cmp::min(inner_buf.len(), inner_buf.remaining());
-        return match buf.write(&inner_buf[..len]) {
-            Ok(len) => {
-                debug!("consumed {} buffered bytes", len);
-                // advance cursor of internal bytes
-                self.consume(len);
-                Poll::Ready(Ok(len))
-            }
-            Err(err) => Poll::Ready(Err(err)),
-        };
-    }
+	fn poll_read(
+		mut self: Pin<&mut Self>,
+		cx: &mut Context<'_>,
+		mut buf: &mut [u8],
+	) -> Poll<Result<usize, std::io::Error>> {
+		log::debug!("poll_read into {} bytes", buf.len());
+		// self is consumed by poll_fill_buf() and we need to advance inner_buf after polling
+		// hence the use of as_mut() to get a new Pin<&mut FieldReader>)
+		let inner_buf = match self.as_mut().poll_fill_buf(cx) {
+			Poll::Ready(Ok(buf)) => buf,
+			Poll::Ready(Err(err)) => return Poll::Ready(Err(err)),
+			Poll::Pending => return Poll::Pending,
+		};
+		// fill buf entirely with field's chunk or fill partially with field's chunk remaining data
+		let len = std::cmp::min(inner_buf.len(), inner_buf.remaining());
+		return match buf.write(&inner_buf[..len]) {
+			Ok(len) => {
+				log::debug!("consumed {} buffered bytes", len);
+				// advance cursor of internal bytes
+				self.consume(len);
+				Poll::Ready(Ok(len))
+			}
+			Err(err) => Poll::Ready(Err(err)),
+		};
+	}
 }
 
 impl AsyncBufRead for FieldReader {
-    fn poll_fill_buf(
-        mut self: Pin<&mut Self>,
-        cx: &mut Context<'_>,
-    ) -> Poll<Result<&[u8], std::io::Error>> {
-        loop {
-            if self.has_chunk() {
-                // This unwrap is very sad, but it can't be avoided.
-                let buf = self.project().chunk.as_ref().unwrap();
-                return Poll::Ready(Ok(buf));
-            } else {
-                return match self.as_mut().project().field.poll_next(cx) {
-                    Poll::Ready(Some(Ok(chunk))) => {
-                        info!("received {} bytes from stream", chunk.remaining());
-                        // Go around the loop in case the chunk is empty.
-                        *self.as_mut().project().chunk = Some(chunk);
-                        continue;
-                    }
-                    Poll::Ready(Some(Err(err))) => Poll::Ready(Err(std::io::Error::new(
-                        std::io::ErrorKind::Other,
-                        err.to_string(),
-                    ))),
-                    Poll::Ready(None) => Poll::Ready(Ok(&[])),
-                    Poll::Pending => Poll::Pending,
-                };
-            }
-        }
-    }
+	fn poll_fill_buf(
+		mut self: Pin<&mut Self>,
+		cx: &mut Context<'_>,
+	) -> Poll<Result<&[u8], std::io::Error>> {
+		loop {
+			if self.has_chunk() {
+				// This unwrap is very sad, but it can't be avoided.
+				let buf = self.project().chunk.as_ref().unwrap();
+				return Poll::Ready(Ok(buf));
+			} else {
+				return match self.as_mut().project().field.poll_next(cx) {
+					Poll::Ready(Some(Ok(chunk))) => {
+						log::debug!("received {} bytes from stream", chunk.remaining());
+						// Go around the loop in case the chunk is empty.
+						*self.as_mut().project().chunk = Some(chunk);
+						continue;
+					}
+					Poll::Ready(Some(Err(err))) => Poll::Ready(Err(std::io::Error::new(
+						std::io::ErrorKind::Other,
+						err.to_string(),
+					))),
+					Poll::Ready(None) => Poll::Ready(Ok(&[])),
+					Poll::Pending => Poll::Pending,
+				};
+			}
+		}
+	}
 
-    fn consume(self: Pin<&mut Self>, amt: usize) {
-        if amt > 0 {
-            self.project()
-                .chunk
-                .as_mut()
-                .expect("No chunk present")
-                .advance(amt);
-        }
-    }
+	fn consume(self: Pin<&mut Self>, amt: usize) {
+		if amt > 0 {
+			self.project()
+				.chunk
+				.as_mut()
+				.expect("No chunk present")
+				.advance(amt);
+		}
+	}
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 mod args;
+mod auth;
 mod config;
 mod fieldreader;
-mod auth;
 
-use crate::{args::Opts, config::Config, fieldreader::FieldReader, auth::TokenAuth};
+use crate::{args::Opts, auth::TokenAuth, config::Config, fieldreader::FieldReader};
 
 use actix_files::{Files, NamedFile};
 use actix_multipart::Multipart;
@@ -11,9 +11,8 @@ use actix_web::{middleware, post, web, App, Error, HttpRequest, HttpResponse, Ht
 use async_compression::futures::bufread::ZstdDecoder;
 use async_tar::Archive;
 use futures::stream::TryStreamExt;
-use log::info;
 use sanitize_filename::sanitize;
-use std::{env, path::Path};
+use std::{env, fs::remove_dir_all, path::Path};
 
 /// upload an unpack a new archive in destination directory. the url is protected by a token
 #[post("/upload", wrap = "TokenAuth")]
@@ -27,15 +26,16 @@ async fn upload(mut payload: Multipart, config: web::Data<Config>) -> Result<Htt
 			.flatten();
 
 		if let Some(filename) = filename {
-			info!("untar {}", filename);
+			log::info!("untar {}", filename);
+			remove_dir_all(&config.upload_to)?;
 			if filename.ends_with(".tar") {
 				Archive::new(FieldReader::new(field))
 					.unpack(&config.upload_to)
-					.await?
+					.await?;
 			} else if filename.ends_with("tar.zst") {
 				Archive::new(ZstdDecoder::new(FieldReader::new(field)))
 					.unpack(&config.upload_to)
-					.await?
+					.await?;
 			}
 		}
 	}
@@ -52,16 +52,10 @@ async fn route_path(req: HttpRequest, config: web::Data<Config>) -> actix_web::R
 }
 
 /// Serve static files on 0.0.0.0:8080
-#[actix_web::main]
 async fn serve(config: Config) -> std::io::Result<()> {
-	env_logger::Builder::new()
-		.parse_filters(
-			&env::var("RUST_LOG".to_owned())
-				.unwrap_or("staticserve=info,actix_web=info".to_owned()),
-		)
-		.init();
 	let addr_port = "0.0.0.0:8080";
-	info!("listening on {}", addr_port);
+	log::info!("listening on {}", addr_port);
+
 	HttpServer::new(move || {
 		App::new()
 			.wrap(middleware::Logger::default())
@@ -69,8 +63,7 @@ async fn serve(config: Config) -> std::io::Result<()> {
 			.data(config.clone())
 			.service(upload)
 			.configure(|cfg| {
-				config.routes.iter().fold(cfg, |cfg, (path, file)| {
-					println!("{} -> {}", path, file);
+				config.routes.iter().fold(cfg, |cfg, (path, _)| {
 					cfg.route(&path, web::get().to(route_path))
 				});
 			})
@@ -82,9 +75,19 @@ async fn serve(config: Config) -> std::io::Result<()> {
 }
 
 fn main() -> anyhow::Result<()> {
+	env_logger::Builder::new()
+		.parse_filters(
+			&env::var("RUST_LOG".to_owned())
+				.unwrap_or("staticserve=info,actix_web=info".to_owned()),
+		)
+		.init();
+
 	let opts: Opts = argh::from_env();
 	// read yaml config
 	let config = Config::read(&opts.config)?;
-	serve(config).unwrap();
+	// start actix main loop
+	let mut system = actix_web::rt::System::new("main");
+	system.block_on::<_, std::io::Result<()>>(serve(config.clone()))?;
+
 	Ok(())
 }
