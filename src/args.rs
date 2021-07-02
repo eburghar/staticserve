d78363fee844ba677133d85bae9c172a8a9170b5
Éric BURGHARD <eric@itsufficient.me>
2021-07-02 10:17:03 +0200

    add default config path

--- src/args.rs
+++ src/args.rs
@@ -3,7 +3,7 @@ use argh::FromArgs;
 #[derive(FromArgs)]
 /// Extract latest projects archives from a gitlab server
 pub struct Opts {
-	#[argh(option, short = 'c')]
+	#[argh(option, short = 'c', default="\"/etc/staticserve.yaml\".to_owned()")]
 	/// configuration file containing projects and gitlab connection parameters
 	pub config: String,
 	#[argh(switch, short = 'v')]
